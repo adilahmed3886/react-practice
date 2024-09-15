67563fe4806d69413140647588fb1e99f3931cfe
adil <adilahmed3886@gmail.com>
2024-09-15 17:11:20 +0500

    Counter Updates

--- 01practice/src/App.jsx
+++ 01practice/src/App.jsx
@@ -1,27 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [count, setCount] = useState(1);
 
   const countUp = () => {
-    count < 20 ? setCount(count + 1) : setCount(count)
-  }
+    count < 20 ? setCount((prevCount) => prevCount + 1) : setCount((prevCount) => prevCount);
+  };
 
   const countDown = () => {
-    count > 0 ? setCount(count -1) : setCount(count)
+    count > 0 ? setCount((prevCount) => prevCount - 1) : setCount((prevCount) => prevCount);
+  };
+
+  const reset = () => {
+    setCount(1)
   }
 
   return (
     <>
-        <h1>{count}</h1>
-        <div className='button'>
-        <button onClick={countUp}>Count +</button>
-        <button onClick={countDown}>Count -</button>
-        </div>
+      <h1 onClick={reset}>{count}</h1>
+      <div className="buttonContainer">
+        <button onClick={countUp} disabled={count >= 20}>Count +</button>
+        <button onClick={countDown} disabled={count <= 0}>Count -</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
